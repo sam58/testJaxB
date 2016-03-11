06d2c112eca856396e6d9922c414c8a5b3163a66
sam58 <sam58@mail.ru>
2016-03-11 08:33:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("D:\\develop\\testJaxB\\src\\test.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(Customer.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Customer customer = (Customer) jaxbUnmarshaller.unmarshal(file);
+            System.out.println(customer);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
